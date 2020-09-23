a1ee3927a94e1d744a7d192d74ac3d50e0e19561
SmilingPlatipus <jpbenoit@laposte.net>
2020-09-23 02:18:45 +0200

    Added transition to LoanActivity

--- app/src/main/java/com/openclassrooms/realestatemanager/activities/LoanActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/activities/LoanActivity.kt
@@ -1,9 +1,10 @@
-package com.openclassrooms.realestatemanager
+package com.openclassrooms.realestatemanager.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.openclassrooms.realestatemanager.R
 
-class SecondActivity : AppCompatActivity() {
+class LoanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loan_simulation)
---
+++ app/src/main/java/com/openclassrooms/realestatemanager/activities/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.openclassrooms.realestatemanager.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.openclassrooms.realestatemanager.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        main_screen?.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                if (p1 == R.id.loan_click_end){
+                    startActivity(Intent(this@MainActivity, LoanActivity::class.java))
+                    finish()
+                }
+            }
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) { }
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) { }
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) { }
+        })
+    }
+}
