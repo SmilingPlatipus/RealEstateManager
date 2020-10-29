f1e1aeec3d4773f9329816a29f6ce746cf9bb503
SmilingPlatipus <jpbenoit@laposte.net>
2020-10-29 20:32:13 +0100

    FEATURES IMPLEMENTED : - SearchActivity getting rangleSliders values OK - recyclerview items design

--- app/src/main/java/com/openclassrooms/realestatemanager/activities/SearchActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/activities/SearchActivity.kt
@@ -1,10 +1,12 @@
 package com.openclassrooms.realestatemanager.activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.constraintlayout.motion.widget.MotionLayout.TransitionListener
 import com.google.android.material.slider.LabelFormatter
+import com.google.android.material.slider.RangeSlider
 import com.openclassrooms.realestatemanager.R
 import kotlinx.android.synthetic.main.activity_search.*
 import java.text.NumberFormat
@@ -12,6 +14,14 @@ import java.util.*
 import kotlin.math.roundToInt
 
 class SearchActivity : AppCompatActivity() {
+    val TAG: String = "SearchActivity"
+
+    var rangeSlider_price_minValue : Float? = null
+    var rangeSlider_price_maxValue : Float? = null
+    var rangeSlider_size_minValue : Float? = null
+    var rangeSlider_size_maxValue : Float? = null
+    var rangeSlider_rooms_minValue : Float? = null
+    var rangeSlider_rooms_maxValue : Float? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +51,46 @@ class SearchActivity : AppCompatActivity() {
                 temp.append(" ")
                 temp.append(getString(R.string.size))
                 return temp.toString()
+            }
+        })
+
+        rangeSlider_price.addOnSliderTouchListener(object : RangeSlider.OnSliderTouchListener{
+            override fun onStopTrackingTouch(slider: RangeSlider) {
+                rangeSlider_price_minValue = slider.values[0]
+                rangeSlider_price_maxValue = slider.values[1]
+
+                Log.d(TAG, "onValueChange: rangeSlider_price_minValue = " + rangeSlider_price_minValue.toString())
+                Log.d(TAG, "onValueChange: rangeSlider_price_maxValue = " + rangeSlider_price_maxValue.toString())
+            }
+
+            override fun onStartTrackingTouch(slider: RangeSlider) {
+
+            }
+        })
+
+        rangeSlider_size.addOnSliderTouchListener(object : RangeSlider.OnSliderTouchListener{
+            override fun onStopTrackingTouch(slider: RangeSlider) {
+                rangeSlider_size_minValue = slider.values[0]
+                rangeSlider_size_maxValue = slider.values[1]
+
+                Log.d(TAG, "onValueChange: rangeSlider_size_minValue = " + rangeSlider_size_minValue.toString())
+                Log.d(TAG, "onValueChange: rangeSlider_size_maxValue = " + rangeSlider_size_maxValue.toString())
+            }
+            override fun onStartTrackingTouch(slider: RangeSlider) {
+
+            }
+        })
+
+        rangeSlider_rooms.addOnSliderTouchListener(object : RangeSlider.OnSliderTouchListener{
+            override fun onStopTrackingTouch(slider: RangeSlider) {
+                rangeSlider_rooms_minValue = slider.values[0]
+                rangeSlider_rooms_maxValue = slider.values[1]
+
+                Log.d(TAG, "onValueChange: rangeSlider_rooms_minValue = " + rangeSlider_rooms_minValue.toString())
+                Log.d(TAG, "onValueChange: rangeSlider_rooms_maxValue = " + rangeSlider_rooms_maxValue.toString())
+            }
+            override fun onStartTrackingTouch(slider: RangeSlider) {
+
             }
         })
     }
